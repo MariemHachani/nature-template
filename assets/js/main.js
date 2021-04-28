9e9e3ebd8a98032702cca98a6ed55b845799ae49
mariem hacheni <hachanimariem16@gmail.com>
2021-04-28 16:22:33 +0100

    carousel

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.fadeOut').owlCarousel({
+        items: 1,
+        animateOut: 'fadeOut',
+        loop: true,
+        margin: 10,
+        autoplay:true,
+      });
+});
